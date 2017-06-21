768e7a5bcdb0977654890df3d6d8ec5b30e4df6f
Isidora Milovanov <isidoramilovanov@gmail.com>
2017-06-21 19:53:10 +0200

    Test map production

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+function myMap() {
+  var mapProp = {
+    center:new google.maps.LatLng(45.822586, 20.454559),
+    zoom:18,
+    scrollwheel: false
+  };
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
