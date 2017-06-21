654ea4795084ddc1095345222aed983931fb7dde
DESKTOP-VKQ6BVT\isako <isakov.cpp@gmail.com>
2017-06-21 23:18:08 +0200

    Google maps integrated.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 
 function myMap() {
-  var mapProp = {
-    center:new google.maps.LatLng(45.822586, 20.454559),
+  var mapProp= {
+    center: new google.maps.LatLng(45.822586, 20.454559),
     zoom:18,
     scrollwheel: false
   };
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var map= new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
